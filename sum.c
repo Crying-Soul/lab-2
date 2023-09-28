10bf96f3d6b12cab4b1380d19eb4a691324e457c
Crying-Soul <grebnevegor2000@mail.ru>
2023-09-28 14:29:36 +0300

    YESSSSSSS

--- sum.c
+++ sum.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int sum(int arr_len, int arr[]){
         int mx = abs_max(arr_len, arr);
-        int s = -1;
+        int s = 0;
 	int f = 0;
         for(int i = 0; i < arr_len; i++){
 		printf("%d %d\n", arr[i], s);
